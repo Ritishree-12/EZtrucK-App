115fdcd3a7828d15e1fc675cb567e93895e0b074
Ritishree-12 <Ritishree1996@gmail.com>
2023-12-28 12:28:59 +0530

    modify the booking screen ui

--- src/screens/trucktypes/TruckTypes.js
+++ src/screens/trucktypes/TruckTypes.js
@@ -36,26 +36,77 @@
 
 // export default TruckTypes;
 
-import React from "react";
+import React,{useState} from "react";
 import { View, Text, Pressable,StyleSheet } from "react-native";
 import TruckRow from './TruckRow';
+import ItemComponent from "../home/ItemComponent";
 
-import typesData from '../../data/type';
+// import typesData from '../../data/type';
 
-const TruckTypes = ({ typeState, onSubmit }) => {
-  const [selectedType, setSelectedType] = typeState;
+const TruckTypes = ({onSubmit}) => {
+  const [selectedId, setSelectedId] = useState();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+ 
+
+    const DATA = [
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "Dala Auto",
+      imgDetails: "13.2ftx6.9ftx5.9ft",
+      capacity: "capacity:900kg",
+      weitage: "Small Pickup",
+      itemDetails:
+        "This vehicle has a large payload capacity of 3.5 tones approximately. capable of carrying construction goods,domestic goods and house/office shifting.",
+      image: require("../../../assets/choose2.png"),
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Tata Ace",
+      imgDetails: "13.2ftx6.9ftx5.9ft",
+      capacity: "capacity:900kg",
+      weitage: "Medium Pickup",
+      itemDetails:
+        "This vehicle has a large payload capacity of 3.5 tones approximately. capable of carrying construction goods,domestic goods and house/office shifting.",
+      image: require("../../../assets/largeVeichle.png"),
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e278",
+      title: "Pickup",
+      imgDetails: "13.2ftx6.9ftx5.9ft",
+      capacity: "capacity:900kg",
+      weitage: "Large Pickup",
+      itemDetails:
+        "This vehicle has a large payload capacity of 3.5 tones approximately. capable of carrying construction goods,domestic goods and house/office shifting.",
+      image: require("../../../assets/choose3.png"),
+    },
+  ];
+  const renderItem = ({ item }) => {
+    const isSelected = item.id === selectedId;
+    const backgroundColor = isSelected ? "#EE272E" : "transparent";
+    const textColor = isSelected ? "white" : "#EE272E";
+
+    return (
+      <ItemComponent
+        item={item}
+        onPress={() => setSelectedId(isSelected ? null : item.id)}
+        backgroundColor={backgroundColor}
+        textColor={textColor}
+      />
+    );
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text1}>Choose a Vehicle</Text>
-      {typesData.map((type) => (
-        <TruckRow
-          type={type}
-          key={type.id}
-          isSelected={type.type === selectedType}
-          onPress={() => setSelectedType(type.type)}
-        />
-      ))}
+    <Text style={styles.text1}>Choose a Vehicle</Text>
+      <TruckRow
+        data={DATA}
+        renderItem={renderItem}
+        selectedId={selectedId}
+        setSelectedId={setSelectedId}
+      />
+      
 
       <Pressable onPress={onSubmit} style={{
        backgroundColor: "#EE272E",
